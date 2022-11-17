e29a8f3c1c9af328ea5629a4d659f1b438bfa23f
pup-pup <reaman.g@gmail.com>
2022-11-17 13:59:38 -0500

    fixed server

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const BASEURL = 'http://localhost:8080/api/';
-export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?poID=';
-// export const BASEURL = '/api/';
-// export const PDFURL = '/PurchaseOrderPDF?poID=';
+// export const BASEURL = 'http://localhost:8080/api/';
+// export const PDFURL = 'http://localhost:8080/PurchaseOrderPDF?poID=';
+export const BASEURL = '/api/';
+export const PDFURL = '/PurchaseOrderPDF?poID=';
